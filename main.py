111a1b5cd9e454425b1967363351ef34700131fe
Aaron Newball <131624869+a-nxwball@users.noreply.github.com>
2024-06-12 18:20:39 -0500

    v0.01

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
-from db_operations import (
-    agregar_receta,
-    actualizar_receta,
-    eliminar_receta,
-    listar_recetas,
-    buscar_ingredientes
-)
+import redis
+
+r_client = redis.Redis(host="localhost", port="6379", db=0)
+
+def agregar_receta(nombre, ingredientes, pasos):
+    id_receta = r_client.incr('next_receta_id')
+    
+    r_client.hmset(f'receta: {id_receta}', {'nombre': nombre, 'ingredientes': ingredientes, 'pasos': pasos})
+    
+    r_client.sadd('recetas', nombre)
+    
+    print('Receta agregada exitosamente.')
+
+def actualizar_receta(id, nombre, ingredientes, pasos):
+    if not r_client.exists(f'receta: {id}'):
+        print(f'Receta con ID: {id}, no encontrada.')
+        return
+    
+    r_client.hmset(f'receta: {id}', {'nombre': nombre, 'ingredientes': ingredientes, 'pasos': pasos})
+    
+    print('Receta actualizada exitosamente.')
+
+def eliminar_receta(id):
+    if not r_client.exists(f'receta: {id}'):
+        print(f'Receta con ID: {id}, no encontrada.')
+        return
+    
+    r_client.delete(f'receta: {id}')
+    r_client.srem('recetas', id)
+    
+    print('Receta eliminada exitosamente.')
+    
+def listar_recetas():
+    recetas = r_client.smembers('recetas')
+    for i, nombre in enumerate(recetas, start = 1):
+        print(f'{i}. {nombre.decode('utf-8')}')
+        
+def buscar_ingredientes(nombre):
+    resultados = []
+    for key in r_client.scan_iter(match = f'receta:*'):
+        receta = r_client.hgetall(key)
+        if nombre.lower() in receta['nombre'].decode('utf-8').lower():
+            resultados.append({
+                'ingredientes': receta['ingredientes'].decode('utf-8'),
+                'pasos': receta['pasos'].decode('utf-8')
+            })
+    return resultados
 
 def main():
     salir = False
